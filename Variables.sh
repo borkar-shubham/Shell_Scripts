a750b52e15bb553d1d3d3fa42596793443be614c
Shubham Borkar <shubham_borkar@outlook.com>
2022-05-18 09:13:04 +0530

    The variables used to assiged some values

--- Variables.sh
+++ Variables.sh
@@ -2,3 +2,11 @@
 var1=Hello
 var2=World
 echo "$var1 $var2"
+
+#Other Examples of Variables..
+#MY_MESSAGE="Hello World"
+#MY_SHORT_MESSAGE=hi
+#MY_NUMBER=1
+#MY_PI=3.142
+#MY_OTHER_PI="3.142"
+#MY_MIXED=123abc
