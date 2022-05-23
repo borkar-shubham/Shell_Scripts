946118b5f071bcee33bc877bbbd39622f51e44e2
Shubham Borkar <shubham_borkar@outlook.com>
2022-05-23 08:19:50 +0530

    This is Interractive script

--- Interractive_Script-1.sh
+++ Interractive_Script-1.sh
@@ -3,7 +3,4 @@ echo "What is your name buddy?"
 read name
 echo "Hii..$name! How are you today?"
 read response
-if
-    {$response==fine}
-then 
-echo "Ohh..Thats great!"
+echo "Oohh..glad to know that you are $response!"
