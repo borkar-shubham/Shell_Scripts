02aa9e1e46642733db141d46c9f6cef3c7efcc69
Shubham Borkar <shubham_borkar@outlook.com>
2022-06-14 15:44:28 +0530

    Case statement with examples

---
+++ case_statements.sh
@@ -0,0 +1,22 @@
+# The case statement is used to execute statements based on specific values.
+# Often used in place of an if statement, if there are a large number of conditions.
+# The variable is compared with the values using the shell wildcards.
+#
+#!/bin/bash
+echo "Enter the type of vehicle to know the rent:"
+read vehicle
+#vehicle=$1  <<-- this exp is used when the input has to be put beside the script name.
+case $vehicle in
+    "car" )
+    echo "The rent of a $vehicle is 50 dollars" ;;
+        "bus" )
+    echo "The rent of a $vehicle is 74 dollars" ;;
+        "jeep" )
+    echo "The rent of a $vehicle is 45 dollars" ;;
+        "truck" )
+    echo "The rent of a $vehicle is 65 dollars" ;;
+        "tractor" )
+    echo "The rent of a $vehicle is 40 dollars" ;;
+    * )
+    echo "You have entered an invalid vehicle type.." ;;
+esac
