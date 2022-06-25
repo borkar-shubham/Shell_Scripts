f736db8dadf819ad93ae45a9a642c064fa92418d
Shubham Borkar <shubham_borkar@outlook.com>
2022-06-25 16:26:53 +0530

    Arithmetic operatioins in shell scripting

--- arithmetic_operations.sh
+++ arithmetic_operations.sh
@@ -9,50 +9,57 @@ echo $(( num1 + num2 ))                #or echo $(expr $num1 + $num2 )
 #
 #Example-2: Subtraction of two numbers
 #!/bin/bash
+echo
 echo "Enther the two numbers to perform subtraction:"
 read num1 num2
 echo $(( num1 - num2 ))                #or echo $(expr $num1 - $num2 )
 #
 #Example-3: Multiplication of two numbers
 #!/bin/bash
+echo
 echo "Enther the two numbers to perform multiplication:"
 read num1 num2
 echo $(( num1 * num2 ))               #or echo $(expr $num1 \* $num2 )
 #
 #Example-4: Division of two numbers
 #!/bin/bash
+echo
 echo "Enther the two numbers to perform division:"
 read num1 num2
 echo $(( num1 / num2 ))
 #
 #Example-5: Modulus of two numbers
 #!/bin/bash
+echo
 echo "Enther the two numbers to get a modulus:"
 read num1 num2
 echo $(( num1 % num2 ))
 #
-#Example-6: Arithmetic operations of two floating numbers
+#Example-6: Square root of two numbers
 #!/bin/bash
+echo
+echo "Enther a number to perfotm square root:"
+read num1
+echo "sqrt( $num1 )" | bc -l
+#
+#Example-7: Cube root of two numbers
+#!/bin/bash
+echo
+echo "Enther a number to perfotm cube root:"
+read num1
+echo "( $num1^3 )" | bc -l
+#
+#Example-8: Num1 to the power Num2
+#!/bin/bash
+echo
+echo "Enther a two numbers to perfotm num1 to the power num2:"
+read num1 num2
+echo "( $num1^$num2 )" | bc -l
+#Example-9: Arithmetic operations of two floating numbers
+#!/bin/bash
+echo
 echo "Enther the two floating numbers to perfotm addition, subtraction & multiplication:"
 read num1 num2
 echo "$num1+$num2" | bc
 echo "$num1-$num2" | bc
 echo "$num1*$num2" | bc
-#
-#Example-7: Square root of two numbers
-#!/bin/bash
-echo "Enther a number to perfotm square root:"
-read num1
-echo "sqrt( $num1 )" | bc -l
-#
-#Example-8: Cube root of two numbers
-#!/bin/bash
-echo "Enther a number to perfotm cube root:"
-read num1
-echo "( $num1^3 )" | bc -l
-#
-#Example-9: Num1 to the power Num2
-#!/bin/bash
-echo "Enther a two numbers to perfotm num1 to the power num2:"
-read num1 num2
-echo "( $num1^$num2 )" | bc -l
