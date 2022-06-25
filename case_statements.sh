68d8d3ecc2c885be3ca863996f67980ce5c36f05
Shubham Borkar <shubham_borkar@outlook.com>
2022-06-25 16:27:52 +0530

    Case statements in shell scripting with examples

--- case_statements.sh
+++ case_statements.sh
@@ -3,19 +3,20 @@
 # The variable is compared with the values using the shell wildcards.
 #
 #!/bin/bash
-echo "Enter the type of vehicle to know the rent:"
+echo "Enter the name of vehicles in the list to know the rent:"
+echo "car, bus, jeep, truck, tractor"
 read vehicle
 #vehicle=$1  <<-- this exp is used when the input has to be put beside the script name.
 case $vehicle in
     "car" )
     echo "The rent of a $vehicle is 50 dollars" ;;
-        "bus" )
+    "bus" )
     echo "The rent of a $vehicle is 74 dollars" ;;
-        "jeep" )
+    "jeep" )
     echo "The rent of a $vehicle is 45 dollars" ;;
-        "truck" )
+    "truck" )
     echo "The rent of a $vehicle is 65 dollars" ;;
-        "tractor" )
+    "tractor" )
     echo "The rent of a $vehicle is 40 dollars" ;;
     * )
     echo "You have entered an invalid vehicle type.." ;;
