4689e9dc36be5f70f6fb48812e0cf3d7f6046086
Shubham Borkar <shubham_borkar@outlook.com>
2022-06-29 06:11:03 +0530

    Working with while loops in shell scripting

--- loops/while_loop.sh
+++ loops/while_loop.sh
@@ -1,10 +1,4 @@
-# Loop is a block of code that is repeated a number of times.
-# The repeating is performed either a pre-determined number of times until a particular condition is satisfied ( while and until loops)
-# To provide flexibility to the loop constructs there are also two statements namely break and continue are provided.
-#
 #!/bin/bash
-#echo "Enter a number to start the while loop:"
-#read num
 n=1
 while [ $n -le 10 ]  #or (( $n <= 10 ))
 do
