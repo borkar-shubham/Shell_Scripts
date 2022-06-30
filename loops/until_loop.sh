97a81dfac7b52c62f1dab50e69692cd8f4a28d6a
Shubham Borkar <shubham_borkar@outlook.com>
2022-06-30 22:04:01 +0530

    Working with 'until loop' in shell scripting

--- loops/until_loop.sh
+++ loops/until_loop.sh
@@ -1,6 +1,6 @@
 #The until structure is very similar to the while structure. 
 #The until structure loops, until the condition becomes true.
-#
+
 #!/bin/bash
 n=1
 until (( $n > 3 )) #here the condition is false..but the will loop execute until its get true..
