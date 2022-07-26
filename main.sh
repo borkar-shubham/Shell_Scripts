99ba015f74302dede8e91697ec86dfce6da0ec4a
Shubham Borkar <shubham_borkar@outlook.com>
2022-07-27 01:31:44 +0530

    This is the main file which has configurations stored in external config file

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source Main-config.sh
+source main-config.sh  #sourcing the config file for calling configurations
 echo "This is $user from the department of $department with the id $id"
