e7c323d9d94e6ad003ccd6c6030fdad28b0cbc00
Shubham Borkar <shubham_borkar@outlook.com>
2022-07-27 01:14:31 +0530

    Variables to define in shell scripting

--- Variables.sh
+++ Variables.sh
@@ -3,6 +3,10 @@ var1=Hello
 var2=World
 echo "$var1 $var2"
 
+#Concatinated variables
+VEHICLE=Land
+echo "The most luxurious vehicle is ${VEHICLE}Rover" #Curly Braces are used to concatenate variables with other words.
+
 #Other Examples of Variables..
 #MY_MESSAGE="Hello World"
 #MY_SHORT_MESSAGE=hi
@@ -10,3 +14,5 @@ echo "$var1 $var2"
 #MY_PI=3.142
 #MY_OTHER_PI="3.142"
 #MY_MIXED=123abc
+
+#Exceptions: A variable cannot be started with a number.
